8ae97a36078fadd96b0fd601db05bb788becd4fd
Muzaffer Onur DAĞHAN <muzaffer@onurdaghan.com>
2018-04-08 16:42:19 +0300

    Question 9 has been solved

---
+++ TZV/TZV.ConUI/Games/2018/Question9.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TZV.ConUI
+{
+    public class Question9
+    {
+         //  Driver program
+        public static void Solution()
+        {
+            //  Create a sample graph
+            Graph g = new Graph(13);
+            g.addEdge(0, 2);
+            g.addEdge(0, 3);
+            g.addEdge(1, 2);
+            g.addEdge(1, 5);
+            g.addEdge(2, 0);
+            g.addEdge(2, 1);
+            g.addEdge(2, 3);
+            g.addEdge(2, 5);
+            g.addEdge(2, 6);
+            g.addEdge(3, 0);
+            g.addEdge(3, 2);
+            g.addEdge(3, 6);
+            g.addEdge(3, 7);
+            g.addEdge(3, 4);
+            g.addEdge(4, 3);
+            g.addEdge(4, 7);
+            g.addEdge(5, 1);
+            g.addEdge(5, 2);
+            g.addEdge(5, 6);
+            g.addEdge(5, 9);
+            g.addEdge(5, 8);
+            g.addEdge(6, 2);
+            g.addEdge(6, 3);
+            g.addEdge(6, 7);
+            g.addEdge(6, 10);
+            g.addEdge(6, 9);
+            g.addEdge(7, 3);
+            g.addEdge(7, 4);
+            g.addEdge(7, 6);
+            g.addEdge(7, 10);
+            g.addEdge(7, 11);
+            g.addEdge(8, 5);
+            g.addEdge(8, 9);
+            g.addEdge(9, 5);
+            g.addEdge(9, 6);
+            g.addEdge(9, 8);
+            g.addEdge(9, 10);
+            g.addEdge(9, 12);
+            g.addEdge(10, 6);
+            g.addEdge(10, 7);
+            g.addEdge(10, 9);
+            g.addEdge(10, 11);
+            g.addEdge(10, 12);
+            g.addEdge(11, 7);
+            g.addEdge(11, 10);
+            g.addEdge(12, 9);
+            g.addEdge(12, 10);
+            //  arbitrary source
+            int s = 0;
+            //  arbitrary destination
+            int d = 12;
+            g.printAllPaths(s, d);
+            Console.WriteLine("Solution : " + g.Count);
+            Console.Read();
+        }
+    }
+}
---
+++ TZV/TZV.ConUI/Helpers/Graphs/Graph.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TZV.ConUI
+{
+    public class Graph
+    {
+
+        //  No. of vertices in graph
+        private int v;
+        //  adjacency list 
+        public List<int>[] adjList;
+        public int Count { get; set; }
+        // Constructor
+        public Graph(int vertices)
+        {
+            // initialise vertex count
+            this.v = vertices;
+            //  initialise adjacency list
+            this.initAdjList();
+        }
+
+        private void initAdjList()
+        {
+            this.adjList = new List<int>[this.v];
+            for (int i = 0; (i < this.v); i++)
+            {
+                this.adjList[i] = new List<int>();
+            }
+
+        }
+
+        //  add edge from u to v
+        public void addEdge(int u, int v)
+        {
+            //  Add v to u's list.
+            adjList[u].Add(v);
+        }
+
+        //  Prints all paths from
+        //  's' to 'd'
+        public void printAllPaths(int s, int d)
+        {
+            Count = 0;
+            bool[] isVisited = new bool[this.v];
+            List<int> pathList = new List<int>();
+            // add source to path[]
+            pathList.Add(s);
+            // Call recursive utility
+            this.printAllPathsUtil(s, d, isVisited, pathList);
+        }
+
+        //  A recursive function to print
+        //  all paths from 'u' to 'd'.
+        //  isVisited[] keeps track of
+        //  vertices in current path.
+        //  localPathList<> stores actual
+        //  vertices in the current path
+        private void printAllPathsUtil(int u, int d, bool[] isVisited, List<int> localPathList)
+        {
+            //  Mark the current node
+            isVisited[u] = true;
+            if (u.Equals(d))
+            {
+                for (int i = 0; i < localPathList.Count(); i++)
+                {
+                    Console.Write(localPathList[i] + " ");
+                }
+                ++Count;
+                Console.WriteLine();
+            }
+
+            //  Recur for all the vertices
+            //  adjacent to current vertex
+            foreach (int i in this.adjList[u])
+            {
+                if (!isVisited[i])
+                {
+                    //  store current node 
+                    //  in path[]
+                    localPathList.Add(i);
+                    this.printAllPathsUtil(i, d, isVisited, localPathList);
+                    //  remove current node
+                    //  in path[]
+                    localPathList.Remove(i);
+                }
+
+            }
+
+            //  Mark the current node
+            isVisited[u] = false;
+        }
+    }
+}
